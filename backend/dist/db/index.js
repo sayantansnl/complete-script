83abeaa350fa7de940af6ec1625305a9d4869aad
sayantansnl <sayantansanyal72@gmail.com>
2026-04-28 20:34:47 +0530

    Stop tracking dist folder

--- backend/dist/db/index.js
+++
@@ -1,6 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema.js";
-import { config } from "../config.js";
-const conn = postgres(config.dbConfig.dbUrl);
-export const db = drizzle(conn, { schema });
