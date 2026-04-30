fc675a4572e5045426f70f999821bce5a8e102ae
sayantansnl <sayantansanyal72@gmail.com>
2026-04-30 20:38:38 +0530

    ErrorPage init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/LoginPage.js";
 import DashboardPage from "./pages/DashboardPage.js";
 import EditorPage from "./pages/EditorPage.js";
 import OutlinePage from "./pages/OutlinePage.js";
+import ErrorPage from "./pages/ErrorPage.js";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/projects/:id" element={<EditorPage />} />
         <Route path="/projects/:id/outline" element={<OutlinePage />} />
+        <Route path="/error" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/ErrorPage.tsx
@@ -0,0 +1,5 @@
+export default function ErrorPage() {
+  return (
+    <h1 className="text-5xl text-red-500 font-bold">404 Page Not Found!</h1>
+  );
+}
